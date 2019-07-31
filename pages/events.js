d75f31c5c0004ef5ae08dc79a7561ad7bd7374c0
Fernando Rama <fernandorama.dev@gmail.com>
2019-07-31 01:01:52 -0300

    Add events resolvers and cards ui changes

--- pages/events.js
+++ pages/events.js
@@ -1,49 +1,159 @@
 import React from 'react';
 import { parseCookies } from 'nookies';
+import moment from 'moment';
 import PageHeader from './PageHeader';
 import Header from '../components/Header';
-import getUserInfo from '../api/getUserInfo';
 import fetchEvents from '../api/fetchEvents';
+
+const transformEventPayload = eventPayload => {
+  console.log(eventPayload);
+  const { type: eventType, actor, payload, repo, created_at } = eventPayload;
+
+  const event = {
+    actor: actor.display_login,
+    actorAvatar: actor.avatar_url,
+    created_at,
+    repoName: repo.name,
+    repoUrl: repo.url.replace(/api\./, '').replace(/repos\//, ''),
+  };
+
+  switch (eventType) {
+    case 'PushEvent':
+      return {
+        ...event,
+        action: 'pushed to',
+        commits: payload.commits,
+        branch: payload.ref.split('/').pop(),
+      };
+    case 'ReleaseEvent':
+      return {
+        ...event,
+        action: `released ${payload.name} of`,
+      };
+    case 'CreateEvent':
+      return {
+        ...event,
+        action: `created ${payload.ref_type} ${payload.ref || ''}`,
+      };
+    case 'WatchEvent':
+      return {
+        ...event,
+        action: 'started watching',
+      };
+    case 'IssuesEvent':
+      return {
+        ...event,
+        action: `${payload.action} an issue on`,
+      };
+  }
+  console.log(`Missing resolver for "${eventType}" event type.`);
+  return null;
+};
+
+const RepositoryCard = (props) => {
+  const { name, description, url } = props;
+  return (
+    <div className="card">
+      <a href={url} target="_blank" className="card-title">{name}</a>
+      <div className="card-description">{description}</div>
+    </div>
+  );
+};
+
+const PushResumeCard = (props) => {
+  const { commits, branch, actorAvatar } = props;
+  const commitsSize = commits.length;
+  const commitsMessage = commitsSize > 1 ? 'commits' : 'commit';
+  return (
+    <div className="card">
+      <pre>{commitsSize} {commitsMessage} to <pre className="branch">{branch}</pre></pre>
+      {commits.slice(0, 2).map((commit, index) => {
+        return (
+          <div className="commit" key={index}>
+            <div className="actor-avatar">
+              <img src={actorAvatar} />
+            </div>
+            <pre className="commit-hash"></pre>
+            <pre className="commit-message">{commit.message}</pre>
+          </div>
+        );
+      })}
+      {commitsSize > 2 && (
+        <a href="">
+          {commitsSize - 2} more commits »
+        </a>
+      )}
+    </div>
+  );
+};
 
 class EventsPage extends React.Component {
   static async getInitialProps(ctx) {
     const cookies = parseCookies(ctx);
     const { token } = cookies;
-    const user = await getUserInfo(token);
-    const events = await fetchEvents({ token, user });
-    return { user, events };
+    const events = await fetchEvents({ token });
+    return { events };
   }
 
   renderEventCards() {
-    const { events, user } = this.props;
-    const {
-      avatar_url: userProfileAvatar,
-      login: userName,
-    } = user;
-    return events.map(({ type }, index) => (
-      <div className="event">
-        <div className="event-header">
-          <div className="user-profile-avatar">
-            <img src={userProfileAvatar} />
+    const { events } = this.props;
+    return events
+      .map(event => transformEventPayload(event))
+      .filter(event => !!event)
+      .map((event, index) => {
+        const {
+          actor,
+          actorAvatar,
+          created_at,
+          repoName,
+          repoUrl,
+          action,
+          commits,
+          branch,
+        } = event;
+        return (
+          <div className="event" key={index}>
+            <div className="event-header">
+              <div className="actor-avatar">
+                <img src={actorAvatar} />
+              </div>
+              <pre className="event-title">
+                <b>{actor}</b> {action} <a href={repoUrl} target="_blank">{repoName}</a> {moment(created_at).fromNow()}
+              </pre>
+            </div>
+            { commits && (
+              <PushResumeCard
+                name={repoName}
+                url={repoUrl}
+                commits={commits}
+                branch={branch}
+                actorAvatar={actorAvatar}
+              />
+            )}
+            { !commits && (
+              <RepositoryCard
+                name={repoName}
+                url={repoUrl}
+                description={""}
+              />
+            )}
           </div>
-          <div className="event-title">{userName}</div>
-        </div>
-        <div className="card" key={index}>
-          <pre className="card-title">{type}</pre>
-        </div>
-      </div>
-    ));
+        );
+      }
+      );
   }
 
   render() {
-    console.log(this.props);
+    const { events } = this.props;
+    const actorAvatar = events[0].actor.avatar_url;
+    const actorName = events[0].actor.display_login;
     return (
       <div>
         <PageHeader />
-        <Header />
+        <Header actorAvatar={actorAvatar} actorName={actorName} />
         <div className="dashboard">
           {this.renderEventCards()}
-          <button className="button">More</button>
+          {/* <button className="button">More</button> */}
         </div>
       </div>
     );
